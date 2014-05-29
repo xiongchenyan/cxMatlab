bde1b05bbfc5766fbbe693216932bca2e2384f2a
chenyanxiong <xiongchenyan@gmail.com>
2014-05-29 17:14:45 -0400

    debug

--- cxKMeans.m
+++ cxKMeans.m
@@ -6,7 +6,7 @@ X=csvread(InName);
 fprintf('read data from [%s] done k=%d\n',InName,k);
 X=spconvert(X);
 fprintf('input size [%d][%d]\n',size(X,1),size(X,2));
-Y=kmeans(X,k,'start','cluster','emptyaction','singleton');
+Y=kmeans(X,k,'emptyaction','singleton');
 csvwrite(OutName,Y);
 fprintf('output [%d][%d] to [%s]\n',size(Y,1),size(Y,2),OutName);
 exit;
