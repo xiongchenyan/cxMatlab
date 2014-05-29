bbb58883312e0ac39205b3f8f1fb60e285f2c55f
chenyanxiong <xiongchenyan@gmail.com>
2014-05-29 15:30:24 -0400

    debug

--- cxKMeans.m
+++ cxKMeans.m
@@ -1,7 +1,9 @@
 function cxKMeans(InName,OutName,k)
 %all input will be in string format
 k = int(sym(k));
+
 X=csvread(InName);
+fprintf('read data from [%s] done k=%d\n',InName,k);
 X=spconvert(X);
 Y=kmeans(X,k);
 csvwrite(OutName,Y);
