e95b33f3d8191cd92cfc21ccbe6d613083f45220
chenyanxiong <xiongchenyan@gmail.com>
2014-05-30 13:33:07 -0400

    add cluster number choose and multiple run for best result

--- cxKMeans.m
+++ cxKMeans.m
@@ -3,13 +3,33 @@ function cxKMeans(InName,OutName,k)
 k = str2num(k);
 
 X=csvread(InName);
-fprintf('read data from [%s] done k=%d\n',InName,k);
+fprintf('read data from [%s] done',InName);
 X=spconvert(X);
-k = min(size(X,1),k);
+%k = min(size(X,1),k);
+%use the rule of thumb to choose k:
+k=min(size(X,1),ceil(sqrt(size(X,1)/2.0)));
+
 fprintf('input size [%d][%d]\n',size(X,1),size(X,2));
-Y=kmeans(X,k,'emptyaction','singleton');
-csvwrite(OutName,Y);
-fprintf('output [%d][%d] to [%s]\n',size(Y,1),size(Y,2),OutName);
+fprintf('k = [%d]\n',k);
+
+%run several times, and pick best Y
+ReRunTime = 10;
+BestY = ones(size(X,1),1);
+MinSumD = -1;
+for i=1:ReRunTime
+    [Y,C,sumd]=kmeans(X,k,'emptyaction','singleton');
+    ThisSum = sum(sumd);
+    if -1 == MinSumD
+        MinSumD = ThisSum;
+    end
+    if MinSumD >= ThisSum
+        MinSumD = ThisSum;
+        fprintf('get a better distance sum: [%f]\n',ThisSum);
+        BestY = Y;
+    end
+end
+csvwrite(OutName,BestY);
+fprintf('output [%d][%d] to [%s]\n',size(BestY,1),size(BestY,2),OutName);
 exit;
 
 
